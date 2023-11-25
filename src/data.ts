afc90b570035e9eb8de87b41805cc6976d619463
amiraallagui <allagui_fawzi@outlook.fr>
2023-11-25 16:14:02 +0100

    data.ts

---
+++ src/data.ts
@@ -0,0 +1,24 @@
+/* eslint-disable prettier/prettier */
+const data :Data = {
+  report: [],
+};
+
+interface Data {
+  report: {
+    id: string;
+    source: string;
+    amount: number;
+    created_at: Date;
+    updated_at: Date;
+    type: string;
+  }[];
+}
+
+data.report.push({
+    id: "uuid",
+    source: "Salary",
+    amount: 7500,
+    created_at: new Date(),
+    updated_at: new Date(),
+    type: "expense",
+})
