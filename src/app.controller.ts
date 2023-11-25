5c85da69fe4c8acb186a4e091f9ebed06b8ed11b
amiraallagui <allagui_fawzi@outlook.fr>
2023-11-25 19:42:51 +0100

    delete and the decorator HttpCode

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,6 +6,7 @@ import {
   Delete,
   Param,
   Body,
+  HttpCode,
 } from '@nestjs/common';
 import { ReportType, data } from './data';
 import { v4 as uuid } from 'uuid';
@@ -66,8 +67,11 @@ export class AppController {
     };
     return data.report[reportIndex];
   }
+  @HttpCode(204)
   @Delete(':id')
-  deleteDelete() {
-    return 'deleted';
+  deleteDelete(@Param('id') id: string) {
+    const reportIndex = data.report.findIndex((report) => report.id === id);
+    if (reportIndex === -1) return;
+    data.report.splice(reportIndex, 1);
   }
 }
