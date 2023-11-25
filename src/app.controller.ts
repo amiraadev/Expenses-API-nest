7f49edbfeb1fb6904850a2574976280de9f5dfed
amiraallagui <allagui_fawzi@outlook.fr>
2023-11-25 21:12:32 +0100

    moving the busines logic from the app.controller to the app.service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,8 +8,7 @@ import {
   Body,
   HttpCode,
 } from '@nestjs/common';
-import { ReportType, data } from './data';
-import { v4 as uuid } from 'uuid';
+import { ReportType } from './data';
 import { AppService } from './app.service';
 
 @Controller('report/:type')
@@ -26,25 +25,16 @@ export class AppController {
   getReportById(@Param('type') type: string, @Param('id') id: string) {
     const reportType =
       type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
-    return data.report
-      .filter((report) => report.type === reportType)
-      .find((report) => report.id === id);
+    return this.appService.getReportById(reportType, id);
   }
   @Post()
   createReport(
     @Param('type') type: string,
     @Body() body: { amount: number; source: string },
   ) {
-    const newReport = {
-      id: uuid(),
-      amount: body.amount,
-      source: body.source,
-      created_at: new Date(),
-      updated_at: new Date(),
-      type: type === 'income' ? ReportType.INCOME : ReportType.EXPENSE,
-    };
-    data.report.push(newReport);
-    return newReport;
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return this.appService.createReport(reportType, body);
   }
 
   @Put(':id')
@@ -55,26 +45,11 @@ export class AppController {
   ) {
     const reportType =
       type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
-    const reportToUpdate = data.report
-      .filter((report) => report.type === reportType)
-      .find((report) => report.id === id);
-
-    if (!reportToUpdate) return;
-    const reportIndex = data.report.findIndex(
-      (report) => report.id === reportToUpdate.id,
-    );
-    data.report[reportIndex] = {
-      ...data.report[reportIndex],
-      ...body,
-      updated_at: new Date(),
-    };
-    return data.report[reportIndex];
+    return this.appService.updateReport(reportType, id, body);
   }
   @HttpCode(204)
   @Delete(':id')
-  deleteDelete(@Param('id') id: string) {
-    const reportIndex = data.report.findIndex((report) => report.id === id);
-    if (reportIndex === -1) return;
-    data.report.splice(reportIndex, 1);
+  deleteReport(@Param('id') id: string) {
+    return this.appService.deleteReport(id);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,52 @@
 import { Injectable } from '@nestjs/common';
 import { ReportType, data } from './data';
+import { v4 as uuid } from 'uuid';
 
+interface ReportData {
+  amount: number;
+  source: string;
+}
 @Injectable()
 export class AppService {
   getAllReports(type: ReportType) {
     return data.report.filter((report) => report.type === type);
+  }
+  getReportById(type: ReportType, id: string) {
+    return data.report
+      .filter((report) => report.type === type)
+      .find((report) => report.id === id);
+  }
+  createReport(type: ReportType, { amount, source }: ReportData) {
+    const newReport = {
+      id: uuid(),
+      amount,
+      source,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type,
+    };
+    data.report.push(newReport);
+    return newReport;
+  }
+  updateReport(type: ReportType, id: string, body: ReportData) {
+    const reportToUpdate = data.report
+      .filter((report) => report.type === type)
+      .find((report) => report.id === id);
+
+    if (!reportToUpdate) return;
+    const reportIndex = data.report.findIndex(
+      (report) => report.id === reportToUpdate.id,
+    );
+    data.report[reportIndex] = {
+      ...data.report[reportIndex],
+      ...body,
+      updated_at: new Date(),
+    };
+    return data.report[reportIndex];
+  }
+  deleteReport(id: string) {
+    const reportIndex = data.report.findIndex((report) => report.id === id);
+    if (reportIndex === -1) return;
+    data.report.splice(reportIndex, 1);
   }
 }
